ec63700db3367345728c57384fb65b795044979e
Andrey Eremin <dsoft88@gmail.com>
2016-09-12 16:40:17 +0200

    Do not execute sharing directly. Use delayed jobs for that

--- index.rb
+++ index.rb
@@ -1,40 +1,21 @@
 # myapp.rb
 require 'sinatra'
-require 'share_to_gplus'
+require 'jobs/share_job'
+require 'sucker_punch'
 
 post '/' do
-  begin
-    status 403 unless post_to_gplus
-  rescue Exception => e
-    puts "Something went wrong: #{e.inspect}"
-  end
+  ShareJob.perform_async(params) if authorised?
+end
+
+get '/' do
+  "Queues: " + SuckerPunch::Queue.all.inspect if authorised?
 end
 
 get '/debug/screenshot' do
-  list = Dir.glob("./*.*").map{|f| f.split('/').last}
-  list2 = Dir.glob("../*.*").map{|f| f.split('/').last}
-  list3 = Dir.glob("/app/./*.*").map{|f| f.split('/').last}
-  #raise list.inspect + list2.inspect + list3.inspect
   file = Dir.glob("/app/./*.*").map{|f| f.split('/').last}.select{|filename| filename.include?('png') }.last
   send_file "/app/./#{file}", :filename => file, :type => 'Application/octet-stream'
 end
 
-# params
-#   secret
-#   link to share
-#   text to share
-#   category to pick
-#   community url
-def post_to_gplus
-  return false if !ENV['SECRET_CODE'] || ENV['SECRET_CODE'] != params['secret']
-  share_this = ShareToGplus::It.new do |config|
-    config.login = ENV['GPLUS_LOGIN']
-    config.password = ENV['GPLUS_PASSWORD']
-    config.text = params['text']
-    config.url =  params['community']
-    config.category = params['category']
-    config.link = params['link']
-    config.javascript_driver = :poltergeist
-  end
-  share_this.execute
+def authorised?
+  (ENV['SECRET_CODE'] && ENV['SECRET_CODE'] == params['secret'])
 end
---
+++ jobs/share_job.rb
@@ -0,0 +1,18 @@
+require 'share_to_gplus'
+
+class ShareJob
+  include SuckerPunch::Job
+
+  def perform(payload)
+      share_this = ShareToGplus::It.new do |config|
+        config.login = ENV['GPLUS_LOGIN']
+        config.password = ENV['GPLUS_PASSWORD']
+        config.text = payload['text']
+        config.url =  payload['community']
+        config.category = payload['category']
+        config.link = payload['link']
+        config.javascript_driver = :poltergeist
+      end
+      share_this.execute
+  end
+end
